5972e0fc53058d83e5942fee54bb5b55b21e95ad
NotDax18 <dax.cw.robinson@gmail.com>
2025-10-18 21:47:06 -0400

    Update SumOfEvenNumbers.java

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
 
 public class SumOfEvenNumbers {
-    public static void main(String[] args) {     
-        // TODO: Step 1 - Get user input
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer value: ");
 
-        // TODO: Step 2 - Validate input
-        
-        // TODO: Step 3 - Calculate sum of even numbers
-        
-        // TODO: Step 4 - Display the result
+        try {
+            int n = scanner.nextInt();
 
+            if (n < 2) {
+                System.out.println("Error: Input must be 2 or greater.");
+                scanner.close();
+                return;
+            }
+
+            int sum = 0;
+            for (int i = 2; i <= n; i += 2) {
+                sum += i;
+            }
+
+            System.out.println("The sum of even numbers between 2 and " + n + " is " + sum + ".");
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Error: Invalid input. Please enter an integer value.");
+
+        }
     }
 }
